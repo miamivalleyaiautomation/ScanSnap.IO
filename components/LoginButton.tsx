6479834ba501ed4e134f45c26227cdbf1f93a237
miamivalleyaiautomation <miamivalleyaiautomation@gmail.com>
2025-09-18 09:21:06 -0400

    Update LoginButton.tsx

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,14 +1,8 @@
-// components/LoginButton.tsx
-‘use client’;
-
 import Link from ‘next/link’;
 
 export default function LoginButton() {
 return (
-<Link 
-href="/sign-in" 
-className="btn primary"
->
+<Link href="/sign-in" className="btn primary">
 Sign In
 </Link>
 );
